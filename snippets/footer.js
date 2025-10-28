2fe8a97f87caa57ac2ce72e19adf7a2d504d15c2
T6970 <tenaldkyf315@gmail.com>
2025-10-28 22:29:14 +0800

    Fix loading footer

--- snippets/footer.js
+++ snippets/footer.js
@@ -1,14 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const footerContainer = document.getElementById("footer");
-  if (footerContainer) {
-    fetch("/footer.html")
-      .then(response => {
-        if (!response.ok) throw new Error("Footer load failed.");
-        return response.text();
-      })
-      .then(html => {
-        footerContainer.innerHTML = html;
-      })
-      .catch(error => console.error("Error loading footer:", error));
-  }
+  fetch("/snippets/footer.html")
+    .then(response => {
+      if (!response.ok) throw new Error("Footer load failed.");
+      return response.text();
+    })
+    .then(footer => {
+      document.body.insertAdjacentHTML(beforeend, footer)
+    })
+    .catch(error => console.error("Error loading footer:", error));
 });
