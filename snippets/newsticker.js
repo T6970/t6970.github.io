4e40c91f606bea179f1aa0f922167a0d1b6b6075
T6970 <tenaldkyf315@gmail.com>
2025-10-29 15:54:42 +0800

    Create newsticker.js
    This selects a random text and put it in news ticker.

---
+++ snippets/newsticker.js
@@ -0,0 +1,17 @@
+(async () => {
+  const ticker = document.getElementById("list");
+
+  try {
+    const news = await fetch("/snippets/news.txt");
+    if (!news.ok) {
+      console.error(new Error("Error fetching news ticker"));
+      return
+    }
+
+    const texts = await news.text();
+    const text  = texts.split("\n")[Math.floor(Math.random()*(texts.split("\n").length))]
+    ticker.innerHTML = `<p class="center">${text}</p>`
+  } catch (e) {
+    console.error(e)
+  }
+})();
