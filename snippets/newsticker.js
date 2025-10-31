7b0a8e1993d9f291fbd20d2943f785bac55d7335
T6970 <tenaldkyf315@gmail.com>
2025-10-31 12:06:09 +0800

    Now the console logs whatever text that is loaded

--- snippets/newsticker.js
+++ snippets/newsticker.js
@@ -4,12 +4,13 @@
   try {
     const news = await fetch("/snippets/news.txt");
     if (!news.ok) {
-      console.error(new Error("Error fetching news ticker"));
+      throw new Error("Error fetching news ticker"));
       return
     }
 
     const texts = await news.text();
-    const text  = texts.split("\n")[Math.floor(Math.random()*(texts.split("\n").length))]
+    const text  = texts.split("\n")[Math.floor(Math.random()*(texts.split("\n").length))];
+    console.log(text);
     ticker.innerHTML = `<p class="center">${text}</p>`
   } catch (e) {
     console.error(e)
